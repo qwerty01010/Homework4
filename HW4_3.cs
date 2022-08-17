a3dbe120d347f7d31b244b6626b1a62ea617a96a
IVANNNNN <gravemail@yandex.ru>
2022-08-17 11:08:08 +0300

    Homework 4_3

---
+++ HW4_3.cs
@@ -0,0 +1,16 @@
+﻿int[] CreateArray()
+{
+    int[] array = new int[8];
+   
+    for (int i = 0; i < 8; i++)
+        {
+            Console.WriteLine("Введите " + i + " элемент массива");
+            array[i] = int.Parse(Console.ReadLine()!);
+        }
+        
+    return array;
+}
+var array = CreateArray();
+
+for (int i = 0; i < 8; i++)
+    Console.Write (" " + array[i]);
