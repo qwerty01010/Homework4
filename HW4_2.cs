e891c52ba7087c9b022862031e031fd07f489634
IVANNNNN <gravemail@yandex.ru>
2022-08-17 11:07:58 +0300

    Homework 4_2

---
+++ HW4_2.cs
@@ -0,0 +1,22 @@
+﻿int Digits (int num)
+{
+    var result = 0;
+     while (num > 0)
+    {
+         result = result + num%10;
+         num = num/10;
+    }
+     return result;
+}
+Console.WriteLine ("Сумма цифр в числе N");
+Console.WriteLine ("Введите число N");
+
+if (int.TryParse(Console.ReadLine()!, out var number))
+{
+  if (number < 0)
+        return;
+    var result = Digits (number);
+    Console.WriteLine (result);
+}
+
+ else {Console.WriteLine ("Введено не число!");}
