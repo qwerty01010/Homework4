585ba700d102f0c738d3b75f9aba800f4dc31b43
IVANNNNN <gravemail@yandex.ru>
2022-08-17 11:07:49 +0300

    Homework 4_1

---
+++ HW4_1.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.WriteLine ("Возведение числа A в натуральную степень B");
+
+Console.WriteLine ("Введите число A");
+int A = int.Parse(Console.ReadLine ()!);
+
+Console.WriteLine ("Введите число B");
+int B = int.Parse(Console.ReadLine ()!);
+
+var result = Math.Pow (A, B);
+Console.WriteLine ("{0:0.00.000}", result);
